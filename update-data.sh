f53c604627a313a2d78f1046dd506a5693c10520
James Bensley <1025875+jwbensley@users.noreply.github.com>
2026-06-21 12:06:22 +0200

    Fix sorting

--- update-data.sh
+++ update-data.sh
@@ -5,9 +5,11 @@ if [ -f gform.csv ]; then
     # Append contents of gform.csv to data.csv
     cat gform.csv >> data.csv
     
-    # Sort data.csv by the second column and overwrite it
-    sort -t, -k2,2 data.csv -o data.csv
-    
+    # Sort data.csv by the second column and overwrite it,
+    # but don't include the first row (headers)
+    (head -n 1 data.csv && tail -n +2 data.csv | sort -t, -g -k2,2) > data_sorted.csv
+    mv data_sorted.csv data.csv
+
     # Remove gform.csv
     rm gform.csv
 else
